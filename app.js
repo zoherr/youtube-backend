1f0f9db8792ed2831d46950b3a6c01a30fa8e3e3
Zoher Rangwala <workforzoher@gmail.com>
2024-09-16 08:43:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+import express from "express";
+import dotenv from "dotenv"
+
+export   const app = express()
+dotenv.config();
---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv"
+
+const app = express()
+dotenv.config();
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
